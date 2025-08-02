fcacc8910c839b5ed12d5dff02f2052a7712f31b
OwlNightist <saxdau@gmail.com>
2025-08-02 20:03:24 +0700

    Update BookingController.cs

--- SnapLink_API/Controllers/BookingController.cs
+++ SnapLink_API/Controllers/BookingController.cs
@@ -185,7 +185,7 @@ public class BookingController : ControllerBase
         }
     }
 
-    [HttpPut("{bookingId}/complete")]
+    [HttpPut("{bookingId}/Confirmed")]
     public async Task<IActionResult> CompleteBooking(int bookingId)
     {
         try
