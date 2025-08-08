fe8627847b419025c3047425caee3b66b9d6b320
Anhtuan <tstnatuan@gmail.com>
2025-08-08 23:10:50 +0700

    add forgetpass verify mail

---
+++ SnapLink_Service/IService/IAuthService.cs
@@ -0,0 +1,17 @@
+﻿using SnapLink_Model.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnapLink_Service.IService
+{
+    public interface IAuthService
+    {
+        Task<string> ForgotPasswordStartAsync(ForgotPasswordRequest req);
+        Task<string> VerifyResetCodeAsync(VerifyResetCodeRequest req);
+        Task<string> ResetPasswordAsync(ResetPasswordRequest req);
+    }
+}
+
